cd15c076997c708d36233c26ce42ba73250b6812
Idan Sidsameri <idan5712@gmail.com>
2019-07-26 12:08:40 +0300

    Add files via upload

--- temp.php
+++ temp.php
@@ -2,6 +2,6 @@
 //this script handle the microconroller http requests.
 include_once 'includes/connection.php';
 
-$str=htmlspecialchars($_GET["name"]);
+$str=htmlspecialchars($_GET["temp"]);
          echo($str."was sent!");
 ?>

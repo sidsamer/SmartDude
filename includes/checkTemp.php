10fdcb1d1f71597d1af699ff88b7368b854a1fb5
Idan Sidsameri <idan5712@gmail.com>
2019-08-23 17:46:21 +0300

    Add files via upload

---
+++ includes/checkTemp.php
@@ -0,0 +1,21 @@
+<?php
+//this script handle the microconroller http requests.
+include_once 'connection.php';
+
+    $sql='SELECT * FROM measurements;';
+    $result=mysqli_query($conn,$sql);
+    $resultCheck=mysqli_num_rows($result);
+    if($resultCheck>0)
+     {
+         $i=0;
+         while($i<$resultCheck)
+         {
+             	    $row=mysqli_fetch_assoc($result);
+             echo "<br> boiler:".$row['boilerTemp']." outside:".$row['outsideTemp'];
+             $i++;
+         }
+	 }
+     else
+     {
+         echo("resultCheck:".$resultCheck);
+     }

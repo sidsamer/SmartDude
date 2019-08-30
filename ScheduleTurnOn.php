7999e3b9155f01fa9310a39eee9b52254bcc5993
Idan Sidsameri <idan5712@gmail.com>
2019-08-30 12:07:23 +0300

    Add files via upload

--- ScheduleTurnOn.php
+++ ScheduleTurnOn.php
@@ -1,20 +1,27 @@
 <?php
 
-// $date=$userId=htmlspecialchars($_GET["userId"]);
-// $temp=shell_exec('wget http://smart-dude.herokuapp.com/Android_req.php/?order=on');
-// echo $temp;
-
 include_once 'includes/connection.php';
-$sql="SELECT * FROM turnon";
+
+$day=strtolower(date("l"));
+$nowTime=date("h:i:s");
+$pastTime=date('H:i:s',strtotime("".$nowTime." -300 seconds"));
+$futureTime=date('H:i:s',strtotime("".$nowTime." +300 seconds"));
+
+$sql="SELECT * FROM turnon where day='$day'";
 	     $result=mysqli_query($conn,$sql);
 	     $resultCheck=mysqli_num_rows($result); 
          if($resultCheck>0)
        {
            while($row=mysqli_fetch_assoc($result))
            {
-               echo $row['userId']." ".$row['showerTime']." ";
+               echo "user:"$row['userId']." time:".$row['showerTime']." regular:".$row['regular']."<br>";
            }
        }
+       echo "day:".$day."<br>";
+echo "time now:".$nowTime."<br>";
+echo "time -5 minutes: ".$pastTime."<br>";
+echo "time +5 minutes: ".$futureTime."<br>";
+
  $temp=shell_exec('wget http://smart-dude.herokuapp.com/Android_req.php/?order=on');
  echo $temp;
 ?>

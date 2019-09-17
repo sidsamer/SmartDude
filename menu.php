d339d7bedbfbb64265c8460fb195e69048e9352b
Idan Sidsameri <idan5712@gmail.com>
2019-09-17 15:34:38 +0300

    Add files via upload

--- menu.php
+++ menu.php
@@ -29,8 +29,8 @@ session_start();
 </form>
 <CENTER>
 <table>
-<td style='padding:15px;'><button class="NewButton" onclick="NoteBody('ScheduleForm');">Create</button></td>
-<td style='padding:15px;'><button class="RemoveButton" onclick="NoteBody('RemoveForm');">Remove</button></td>
+<td style='padding:30px;'><button class="NewButton" onclick="NoteBody('ScheduleForm');">Create</button></td>
+<td style='padding:30px;'><button class="RemoveButton" onclick="NoteBody('RemoveForm');">Remove</button></td>
 </table>
 <br><br><br>
 <div class="ScheduleForm"; id="ScheduleForm"; style="display:none;">
